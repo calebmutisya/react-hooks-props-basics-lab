3ce555ab50f2b2417eb183b15b3904ee0df93a1d
calebmutisya <mutisyacalebmusau@gmail.com>
2023-10-25 16:58:46 +0300

    Completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,23 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+import user from "../data/user";
+function About(props) {
+  if (!props.bio) {
+    // hide unpublished content
+    // return null means "don't display any DOM elements here"
+    return null;
+    } else {
+    // show published content
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <p>{props.bio}</p>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links github={user.links.github} linkedin={user.links.linkedin}/>
+      </div>
+    );
+  }
 }
 
 export default About;
